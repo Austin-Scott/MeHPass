0e870fdca539e04a1ef5c1a9f2b642ae82522b53
theMetagame <16288@msd.k12.wi.us>
2016-12-22 15:12:08 -0600

    Added basic functionality for passwords up to 3 characters in length

--- mehpass/main.cpp
+++ mehpass/main.cpp
@@ -1,23 +1,131 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <locale>
 
 using namespace std;
 
-void loadFiles()   {
+fstream fNouns;
+vector<string> Nouns;
+fstream fVerbs;
+vector<string> Verbs;
+fstream fAdjectives;
+vector<string> Adjectives;
+locale loc;
 
+
+int randInt(int lowest, int highest)   {
+    return rand()%((highest+1)-lowest)+lowest;
+}
+
+int loadFiles()   {
+    fNouns.open("noun.txt");
+    if(fNouns.is_open())   {
+        while(!fNouns.eof())  {
+            string read="";
+            getline(fNouns,read);
+            Nouns.push_back(read);
+        }
+        fNouns.close();
+
+        fVerbs.open("verbs.txt");
+        if(fVerbs.is_open())   {
+            while(!fVerbs.eof())    {
+            string read="";
+            getline(fVerbs,read);
+            Verbs.push_back(read);
+        }
+
+            fVerbs.close();
+
+            fAdjectives.open("adjectives.txt");
+            if(fAdjectives.is_open())   {
+                while(!fAdjectives.eof())   {
+            string read="";
+            getline(fAdjectives,read);
+            Adjectives.push_back(read);
+        }
+
+                fAdjectives.close();
+
+            } else  {
+                cout<<"Error opening \"adjectives.txt\""<<endl;
+                return 1;
+            }
+        } else {
+            cout<<"Error opening \"verbs.txt\""<<endl;
+            return 1;
+        }
+    } else {
+        cout<<"Error opening \"noun.txt\""<<endl;
+        return 1;
+    }
+    return 0;
+}
+
+string gfc(string input)   { //return the first character of input in the range a-z converting numbers into letter Ex: 3 to e
+    //TODO:write this function
+    string result = input;
+    string lowercase="";
+    for(string::size_type i=0;i<result.length();i++)   {
+    lowercase+=tolower(result[i],loc);
+    }
+    result=lowercase;
+
+    return result;
+}
+
+string getNoun(string input)   {
+    vector<string> selections;
+    for(int i=0;i<Nouns.size();i++)   {
+        if(Nouns.at(i).substr(0,1).compare(input)==0)   {
+            selections.push_back(Nouns.at(i));
+        }
+    }
+    return selections.at(randInt(0,selections.size()-1));
+}
+
+string getVerb(string input)   {
+    vector<string> selections;
+    for(int i=0;i<Verbs.size();i++)   {
+        if(Verbs.at(i).substr(0,1).compare(input)==0)   {
+            selections.push_back(Verbs.at(i));
+        }
+    }
+    return selections.at(randInt(0,selections.size()-1));
+}
+
+string getAdjective(string input)    {
+    vector<string> selections;
+    for(int i=0;i<Adjectives.size();i++)   {
+        if(Adjectives.at(i).substr(0,1).compare(input)==0)   {
+            selections.push_back(Adjectives.at(i));
+        }
+    }
+    return selections.at(randInt(0,selections.size()-1));
 }
 
 string Convert(string input)    {
-    string result=input;
+    string result="";
+    if(input.length()==1)   {
+        result+="The "+getNoun(gfc(input.substr(0,1)))+".";
+    } else if(input.length()==2)   {
+        result+="The "+getAdjective(gfc(input.substr(0,1)))+" "+getNoun(gfc(input.substr(1,1)))+".";
+    } else if(input.length()==3)   {
+        result+="The "+getAdjective(gfc(input.substr(0,1)))+" "+getNoun(gfc(input.substr(1,1)))+" will "+getVerb(gfc(input.substr(2,1)))+".";
+    }
     return result;
 }
 
 int main(int argc, char* argv[])   {
-    cout<<"Welcome to Memorize Hard Passwords or MeHPass for short!"<<endl;
+    srand(time(nullptr));
     if(argc!=2)   {
         cout<<"Proper usage: \"mehpass.exe password\""<<endl;
     } else {
-        loadFiles();
-        cout<<"Generated mnemonic for "<<argv[1]<<":"<<endl<<Convert(argv[1])<<endl;
+        if(loadFiles()==0)
+        cout<<"Generated mnemonic for "<<argv[1]<<":"<<endl<<"\""<<Convert(argv[1])<<"\""<<endl;
     }
 }
