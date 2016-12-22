a6af659d0d34f20deb95c39583aa4440dd96df0e
theMetagame <16288@msd.k12.wi.us>
2016-12-21 23:41:56 -0600

    Templated what I am going to be working on in the future

--- mehpass/main.cpp
+++ mehpass/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void loadFiles()   {
+
+}
+
+string Convert(string input)    {
+    string result=input;
+    return result;
+}
+
 int main(int argc, char* argv[])   {
-    cout<<"Hello World!"<<endl;
+    cout<<"Welcome to Memorize Hard Passwords or MeHPass for short!"<<endl;
+    if(argc!=2)   {
+        cout<<"Proper usage: \"mehpass.exe password\""<<endl;
+    } else {
+        loadFiles();
+        cout<<"Generated mnemonic for "<<argv[1]<<":"<<endl<<Convert(argv[1])<<endl;
+    }
 }
